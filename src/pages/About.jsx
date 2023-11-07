2959a56248b271cd46969f4daff4f2cf49858c10
Gurpreet <gurpreetkaurthegreat@gmail.com>
2023-11-08 00:41:00 +0530

    made expandable navbar menu

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,13 +1,20 @@
-import React from 'react'
+import React from 'react';
+import './About.css';
+import ExpandableMenuItem from '../components/StudentZone_MenuBar.jsx';
 
-  const About = () => {
+const items = Array(10).fill().map((_, index) => ({
+  title: `Menu ${index + 1}`,
+  paragraph: `Description for Menu ${index + 1}.`,
+}));
+
+function App() {
   return (
-    <>
-    <div className='mt-16'>
-      This will be made by gurpreet
+    <div className="App">
+      {items.map((item, index) => (
+        <ExpandableMenuItem key={index} {...item} />
+      ))}
     </div>
-    </>
-  )
+  );
 }
 
-export default About
+export default App;
